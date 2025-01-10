0880b5bb8d3cff855ceeb3c462ce3d25874fa742
FlorianBarkmann <fbarkmann@ethz.ch>
2025-01-10 14:11:37 +0100

    logging

--- cancerfoundation/trainer.py
+++ cancerfoundation/trainer.py
@@ -530,7 +530,8 @@ class Trainer:
                         loss_conditions /= len(self.conditions)
                         loss += loss_conditions
 
-                if self.USE_GENERATIVE_TRAINING and global_iter > 1000:
+                if self.USE_GENERATIVE_TRAINING and global_iter > 1000:#
+                    print("Bla")
                     previous_cell_embs = output_dict["cell_emb"].detach()
                     preds = self.model(
                         pcpt_gene,
@@ -547,7 +548,6 @@ class Trainer:
                     loss_gen = criterion(
                         preds, gen_expr_target, positions_to_match)
                     loss = loss + loss_gen
-                loss += 0. * torch.sum(p.sum for p in self.model.mvc_decoder.parameters())
                 self.accelerator.backward(loss)
 
                 if self.accelerator.sync_gradients:
