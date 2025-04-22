6f7f9edad8276d127e9ed1deb9ae1856d4ff6161
florianbarkmann <flobarkmann@googlemail.com>
2025-04-22 17:29:29 +0000

    fix: update launch and job scripts for improved training configuration and multi-node support

--- submits/debug_2_nodes.sh
+++ submits/debug_2_nodes.sh
@@ -16,30 +16,16 @@ LAYERS=6
 EMBSIZE=256
 JOB_NAME="debug"
 
-export MASTER_ADDR=$(scontrol show hostnames $SLURM_JOB_NODELIST | head -n 1)
-export MASTER_PORT=$(( RANDOM % (50000 - 30000 + 1 ) + 30000 ))
-export GPUS_PER_NODE=$SLURM_GPUS_PER_NODE
-export NNODES=$SLURM_NNODES
-export NUM_PROCESSES=$(expr $NNODES \* $GPUS_PER_NODE)
-export MULTIGPU_FLAG="--multi_gpu"
-
-if [ $NNODES == "1" ]
-then
-        export MULTIGPU_FLAG=""
-fi
-
-echo "MASTER_ADDR: $MASTER_ADDR"
-echo "MASTER_PORT: $MASTER_PORT"
-echo "Using $NNODES nodes, $NUM_PROCESSES GPUs total"
+export GPUS_PER_NODE=4
+head_node_ip=$(scontrol show hostnames $SLURM_JOB_NODELIST | head -n 1)
 
 srun --environment=bionemo accelerate launch \
-    --main_process_ip ${MASTER_ADDR} \
-    --main_process_port ${MASTER_PORT} \
-    --machine_rank $SLURM_NODEID \
-    $MULTIGPU_FLAG \
-    --same_network \
-    --num_processes $NUM_PROCESSES \
-    --num_machines $NNODES \
+    --num_processes $((SLURM_NNODES * GPUS_PER_NODE)) \
+    --num_machines $SLURM_NNODES \
+    --machine_rank $SLURM_PROCID \
+    --rdzv_backend c10d \
+    --main_process_ip $head_node_ip \
+    --main_process_port 29501 \
     --mixed_precision bf16 \
     ./pretrain.py \
     --save-dir ./save/$JOB_NAME-$(date +%b%d-%H-%M-%Y) \
