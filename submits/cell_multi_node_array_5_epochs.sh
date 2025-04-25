a5beebb9709b4975dc61fe9934a26133378c1d3a
florianbarkmann <flobarkmann@googlemail.com>
2025-04-25 09:29:06 +0000

    update submit script

--- submits/cell_multi_node_array_5_epochs.sh
+++ submits/cell_multi_node_array_5_epochs.sh
@@ -1,10 +1,9 @@
 #!/bin/bash -l
 #SBATCH --job-name=cf-pretrain
-#SBATCH --nodes=4
+#SBATCH --nodes=16
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=6:00:00
+#SBATCH --time=12:00:00
 #SBATCH --gpus-per-node=4
-#SBATCH --array [0-4]%1
 
 
 
@@ -15,15 +14,13 @@ per_proc_batch_size=16
 LAYERS=6
 EMBSIZE=256
 JOB_NAME="debug"
-SAVE_DIR="./save/cell_5_epochs"
+SAVE_DIR="./save/cell_5_epoch_new"
 export GPUS_PER_NODE=4
 
-CURRENT_EPOCH=$SLURM_ARRAY_TASK_ID
+
 
 head_node_ip=$(scontrol show hostnames $SLURM_JOB_NODELIST | head -n 1)
 
-if [ $CURRENT_EPOCH -eq 0 ]; then
-  echo "Running first epoch (epoch $CURRENT_EPOCH)"
 
 srun --environment=bionemo accelerate launch \
     --num_processes $((SLURM_NNODES * GPUS_PER_NODE)) \
@@ -31,7 +28,7 @@ srun --environment=bionemo accelerate launch \
     --machine_rank $SLURM_PROCID \
     --rdzv_backend c10d \
     --main_process_ip $head_node_ip \
-    --main_process_port 29502 \
+    --main_process_port 29511 \
     --mixed_precision bf16 \
     ./pretrain.py \
     --save-dir $SAVE_DIR \
@@ -43,7 +40,6 @@ srun --environment=bionemo accelerate launch \
     --embsize $EMBSIZE \
     --d-hi 512 \
     --epochs 5 \
-    --num-epochs 1 \
     --lr 0.0001 \
     --warmup-ratio-or-step 10000 \
     --log-interval $LOG_INTERVAL \
@@ -57,20 +53,3 @@ srun --environment=bionemo accelerate launch \
     --do-dat \
     --wandb "fulldata"
 
-else
-PREV_EPOCH=$((CURRENT_EPOCH - 1))
-CHECKPOINT_PATH="$SAVE_DIR/epoch_$PREV_EPOCH"
-
-srun --environment=bionemo accelerate launch \
-    --num_processes $((SLURM_NNODES * GPUS_PER_NODE)) \
-    --num_machines $SLURM_NNODES \
-    --machine_rank $SLURM_PROCID \
-    --rdzv_backend c10d \
-    --main_process_ip $head_node_ip \
-    --main_process_port 29502 \
-    --mixed_precision bf16 \
-    ./pretrain.py \
-    --resume-from-checkpoint $CHECKPOINT_PATH \
-    --num-epochs 1
-
-fi
