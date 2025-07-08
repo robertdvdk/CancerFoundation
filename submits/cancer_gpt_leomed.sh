8690c9a7d9a85020bbb8b92a3da0b6eaf9518ee4
florianbarkmann <flobarkmann@googlemail.com>
2025-07-08 11:34:24 +0000

    new submitscript for leomed

--- submits/cancer_gpt_leomed.sh
+++ submits/cancer_gpt_leomed.sh
@@ -3,7 +3,7 @@
 
 singularity run --pwd /cluster/work/boeva/fbarkmann/CancerFoundation --bind /cluster/work/boeva/fbarkmann/CancerFoundation:/cluster/work/boeva/fbarkmann/CancerFoundation --nv /cluster/customapps/biomed/boeva/fbarkmann/bionemo-framework_nightly.sif python pretrain.py \
     --gpus 4 \
-    --save-dir ./save/cancergpt-$(date +%b%d-%H-%M-%Y) \
+    --save-dir ./save/cf_oversampling-$(date +%b%d-%H-%M-%Y) \
     --max-seq-len 1200 \
     --batch-size 32 \
     --nlayers 6 \
@@ -14,12 +14,13 @@ singularity run --pwd /cluster/work/boeva/fbarkmann/CancerFoundation --bind /clu
     --lr 0.0001 \
     --warmup-ratio-or-step 10000 \
     --grad-accu-steps 2 \
+    --val-check-interval 0.5 \
     --trunc-by-sample \
-    --loss "mse" \
-    --train-path "./training_data/pretraining_data/cancer_gpt" \
+    --loss mse \
+    --balance-primary tissue \
+    --balance-secondary technology \
+    --train-path training_data/pretraining_data/cancer_gpt \
+    --wandb cancer_gpt \
+    --wandb-entity cancerfoundation \
     --zero-percentages 0.2 0.4 0.6 \
-    --conditions "technology" \
-    --balance-primary "tissue" \
-    --balance-secondary "technology" \
-    --wandb "cells" \
-    --strategy "ddp_find_unused_parameters_true"
+    --strategy='ddp_find_unused_parameters_true'
