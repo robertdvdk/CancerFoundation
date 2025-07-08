d4031c709d1ddd4578d83676c294ced327aef5f2
florianbarkmann <flobarkmann@googlemail.com>
2025-07-08 18:46:52 +0000

    fixed ddp

--- submits/cf_oversample.sh
+++ submits/cf_oversample.sh
@@ -25,4 +25,4 @@ srun --environment=bionemo python pretrain.py \
     --wandb cancer_gpt \
     --wandb-entity cancerfoundation \
     --zero-percentages 0.2 0.4 0.6 \
-    --strategy='ddp_find_unused_parameters_true'
+    --strategy='ddp'
