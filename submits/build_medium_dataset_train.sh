4351420bf74c42198205b67f427c66251543d43c
Robert van der Klis <robert.vdklis@protonmail.com>
2025-08-20 19:24:42 +0000

    build medium-size dataset

--- submits/build_medium_dataset_train.sh
+++ submits/build_medium_dataset_train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #SBATCH --job-name=create_medium_dataset_train
 #SBATCH --output=./%x_%j.out
-#SBATCH --time=00:30:00
+#SBATCH --time=01:00:00
 #SBATCH --partition=gpu
 #SBATCH --ntasks-per-node=1
 #SBATCH --gpus-per-task=1
