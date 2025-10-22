8c000bfd7240f774d06369928a502559335cdf3c
Robert van der Klis <robert.vdklis@protonmail.com>
2025-10-22 12:53:45 +0000

    bugfix again..

--- gene_perturbation/submits_cscs/evaluate_gene_perturbation_seed3.sh
+++ gene_perturbation/submits_cscs/evaluate_gene_perturbation_seed3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 #SBATCH --job-name=evaluate_gene_perturbation_3
-#SBATCH --output=./%x_%j.out
+#SBATCH --output=./gene_perturbation/%x_%j.out
 #SBATCH --time=01:00:00
 #SBATCH --partition=normal
 #SBATCH --ntasks-per-node=1
@@ -12,20 +12,21 @@ set -x
 
 ulimit -c 0
 
-srun -ul --environment=./bionemo_clariden.toml bash -c "
+srun -ul --environment=../bionemo_clariden.toml bash -c "
     pip install cell-gears==0.0.2
+    cd /users/rvander/my_prop/CancerFoundation/gene_perturbation
     MASTER_ADDR=\$(scontrol show hostnames \$SLURM_JOB_NODELIST | head -n 1) \
     MASTER_PORT=29500 \
     RANK=\${SLURM_PROCID} \
     LOCAL_RANK=\${SLURM_LOCALID} \
     WORLD_SIZE=\${SLURM_NTASKS} \
-    python ./gene_perturbation/run_perturbation.py \
-    --checkpoint ./save/train_medium_condtech_my_init_weights_4gpu_lrx2_955577/epoch_epoch=14.ckpt \
+    python run_perturbation.py \
+    --checkpoint ../save/train_medium_condtech_my_init_weights_4gpu_lrx2_955577/epoch_epoch=14.ckpt \
     --data-name adamson \
     --epochs 15 \
     --lr 1e-4 \
     --batch-size 16 \
     --eval-batch-size 16 \
-    --save-dir ./gene_perturbation/my_results/myinit_lrx2_seed3 \
+    --save-dir ./my_results/myinit_lrx2_seed3 \
     --seed 3
 "
